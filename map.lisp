0f057e65232cc82977963c38e4ace1589d56736e
Yakov Zaytsev <yakov@clickgrab.net>
2012-07-13 17:11:12 +0400

    COLS/ROWS

--- map.lisp
+++ map.lisp
@@ -4,3 +4,10 @@
 
 (defun empty-map (cols rows)
   (make-array `(,rows ,cols) :element-type '(unsigned-byte 8)))
+
+(defmacro cols (map)
+  `(array-dimension ,map 1))
+
+(defmacro rows (map)
+  `(array-dimension ,map 0))
+
